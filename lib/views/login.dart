cc389c77e6b04f726093e949529a7eb7f84773b7
BilalAzeem210 <biazeem210@gmail.com>
2024-07-09 09:57:17 +0500

    new changes

---
+++ lib/views/login.dart
@@ -0,0 +1,28 @@
+import'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/loginback.png',
+            ),
+            fit: BoxFit.cover
+          )
+        ),
+        child: const Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
